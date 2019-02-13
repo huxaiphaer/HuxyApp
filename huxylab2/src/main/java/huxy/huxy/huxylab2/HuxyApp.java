2d9a2fa7afbe4c965d544987f1bfc8a91cc009eb
huxaiphaer <l654321123>
2019-02-13 15:28:52 +0300

    (Feat)  - Add a success message of a toast.

--- huxylab2/src/main/java/huxy/huxy/huxylab2/HuxyApp.java
+++ huxylab2/src/main/java/huxy/huxy/huxylab2/HuxyApp.java
@@ -12,7 +12,7 @@ import android.widget.Toast;
 public class HuxyApp extends AppCompatActivity {
 
 
-    public static void successToast(Activity activity,String message, int position, int xOffset, int yOffset) {
+    public static void successToast(Activity activity, String message, int position, int xOffset, int yOffset) {
 
         LayoutInflater inflater = activity.getLayoutInflater();
         View layout = inflater.inflate(R.layout.lib_activity_main,
@@ -20,6 +20,46 @@ public class HuxyApp extends AppCompatActivity {
 
         layout.setBackgroundColor(activity
                 .getResources().getColor(R.color.successMessage));
+
+        TextView text = layout.findViewById(R.id.header1);
+        text.setText(message);
+
+        Toast toast = new Toast(activity);
+        toast.setGravity(position, xOffset, yOffset);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(layout);
+        toast.show();
+
+    }
+
+    public static void dangerToast(Activity activity, String message, int position, int xOffset, int yOffset) {
+
+        LayoutInflater inflater = activity.getLayoutInflater();
+        View layout = inflater.inflate(R.layout.lib_activity_main,
+                (ViewGroup) activity.findViewById(R.id.custom_toast_container));
+
+        layout.setBackgroundColor(activity
+                .getResources().getColor(R.color.dangerMessage));
+
+        TextView text = layout.findViewById(R.id.header1);
+        text.setText(message);
+
+        Toast toast = new Toast(activity);
+        toast.setGravity(position, xOffset, yOffset);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(layout);
+        toast.show();
+
+    }
+
+    public static void warningToast(Activity activity, String message, int position, int xOffset, int yOffset) {
+
+        LayoutInflater inflater = activity.getLayoutInflater();
+        View layout = inflater.inflate(R.layout.lib_activity_main,
+                (ViewGroup) activity.findViewById(R.id.custom_toast_container));
+
+        layout.setBackgroundColor(activity
+                .getResources().getColor(R.color.warningMessage));
 
         TextView text = layout.findViewById(R.id.header1);
         text.setText(message);
