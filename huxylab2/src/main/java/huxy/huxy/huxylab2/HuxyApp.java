f6ce3ade6a827cc5ba2e2570869ca9a2baf5e4a2
huxaiphaer <l654321123>
2019-02-14 23:18:52 +0300

    (feat) add method chaining. - This task is to ensure that method chaining is implemented.

--- huxylab2/src/main/java/huxy/huxy/huxylab2/HuxyApp.java
+++ huxylab2/src/main/java/huxy/huxy/huxylab2/HuxyApp.java
@@ -11,65 +11,121 @@ import android.widget.Toast;
 
 public class HuxyApp extends AppCompatActivity {
 
+    private final static HuxyApp INSTANCE = new HuxyApp();
+    static LayoutInflater inflater;
+    static Toast toast;
+    static TextView text;
 
-    public static void successToast(Activity activity, String message, int position, int xOffset, int yOffset) {
+    /**
+     * This method is capable for showing the success toast message.
+     *
+     * @param activity This is the activity.
+     * @param message  This is the message of the toast.
+     * @return This returns the instance of the class.
+     */
+    public static HuxyApp successToast(Activity activity, String message) {
 
-        LayoutInflater inflater = activity.getLayoutInflater();
+        inflater = activity.getLayoutInflater();
         View layout = inflater.inflate(R.layout.lib_activity_main,
                 (ViewGroup) activity.findViewById(R.id.custom_toast_container));
 
         layout.setBackgroundColor(activity
                 .getResources().getColor(R.color.successMessage));
 
-        TextView text = layout.findViewById(R.id.header1);
+        text = layout.findViewById(R.id.header1);
         text.setText(message);
 
-        Toast toast = new Toast(activity);
-        toast.setGravity(position, xOffset, yOffset);
+        toast = new Toast(activity);
+
         toast.setDuration(Toast.LENGTH_LONG);
         toast.setView(layout);
         toast.show();
 
+        return INSTANCE;
+
     }
 
-    public static void dangerToast(Activity activity, String message, int position, int xOffset, int yOffset) {
+    /**
+     * This is the method for showing a danger toast message.
+     *
+     * @param activity This is the activity.
+     * @param message  This is the message of the toast.
+     * @return This returns the instance of the class.
+     */
+    public static HuxyApp dangerToast(Activity activity, String message) {
 
-        LayoutInflater inflater = activity.getLayoutInflater();
+        inflater = activity.getLayoutInflater();
         View layout = inflater.inflate(R.layout.lib_activity_main,
                 (ViewGroup) activity.findViewById(R.id.custom_toast_container));
 
         layout.setBackgroundColor(activity
                 .getResources().getColor(R.color.dangerMessage));
 
-        TextView text = layout.findViewById(R.id.header1);
+        text = layout.findViewById(R.id.header1);
         text.setText(message);
 
-        Toast toast = new Toast(activity);
-        toast.setGravity(position, xOffset, yOffset);
+        toast = new Toast(activity);
+
         toast.setDuration(Toast.LENGTH_LONG);
         toast.setView(layout);
         toast.show();
 
+        return INSTANCE;
+
     }
 
-    public static void warningToast(Activity activity, String message, int position, int xOffset, int yOffset) {
+    /**
+     * This method is responsible for displaying the warning toast message.
+     *
+     * @param activity This is the activity.
+     * @param message  This is the message of the toast.
+     * @return Return the instance of the class.
+     */
+    public static HuxyApp warningToast(Activity activity, String message) {
 
-        LayoutInflater inflater = activity.getLayoutInflater();
+        inflater = activity.getLayoutInflater();
         View layout = inflater.inflate(R.layout.lib_activity_main,
                 (ViewGroup) activity.findViewById(R.id.custom_toast_container));
 
         layout.setBackgroundColor(activity
                 .getResources().getColor(R.color.warningMessage));
 
-        TextView text = layout.findViewById(R.id.header1);
+        text = layout.findViewById(R.id.header1);
         text.setText(message);
 
-        Toast toast = new Toast(activity);
-        toast.setGravity(position, xOffset, yOffset);
+        toast = new Toast(activity);
+
         toast.setDuration(Toast.LENGTH_LONG);
         toast.setView(layout);
         toast.show();
 
+        return INSTANCE;
+
+    }
+
+    /**
+     * This method is capable for setting the position and offsets.
+     *
+     * @param position This aligns the toast position.
+     * @param xOffset  Sets the x offset.
+     * @param yOffset  Sets the y offset.
+     * @return returns the instance of the class.
+     */
+    public static HuxyApp setPositionAndOffSet(int position, int xOffset, int yOffset) {
+
+        toast.setGravity(position, xOffset, yOffset);
+        return INSTANCE;
+    }
+
+    public static HuxyApp setTextSize(float size) {
+
+        text.setTextSize(size);
+        return INSTANCE;
+    }
+
+    public static HuxyApp setPadding(int padding) {
+        text.setPadding(padding, padding, padding, padding);
+        return INSTANCE;
     }
 
 
